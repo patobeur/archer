149ac616c32207d3c81921a3dc49e7a8dd48bc6c
Patobeur <patobeur41@gmail.com>
2025-09-28 13:35:50 +0200

    modified:   js/archer/archer.js 	new file:   js/config.js 	new file:   js/data/nature_positions.json 	new file:   js/modules/populate_nature.js 	new file:   js/modules/procedural_generator.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const config = {
+	// 'json' pour charger depuis le fichier nature_positions.json
+	// 'procedural' pour générer le monde à la volée
+	population_mode: "procedural",
+};
+
+export { config };
---
+++ js/modules/procedural_generator.js
@@ -0,0 +1,65 @@
+const _proceduralGenerator = {
+	generateNatureObjects: function () {
+		console.log(
+			"[procedural_generator.js] Generating nature objects procedurally..."
+		);
+		const natureObjects = [];
+
+		const models = [
+			"BigTreeWithLeaves",
+			"SmallTreeWithLeave",
+			"TreeNoLeavesBig",
+			"TreeNoLeavesSmall",
+			"BigBush",
+			"SmallBush",
+		];
+
+		const groveCenters = [
+			{ x: -100, z: -80, density: 155, radius: 60 },
+			{ x: 120, z: -150, density: 132, radius: 65 },
+			{ x: 60, z: 100, density: 125, radius: 65 },
+			{ x: -170, z: 140, density: 145, radius: 60 },
+			{ x: 190, z: 20, density: 150, radius: 65 },
+			{ x: 0, z: -180, density: 130, radius: 50 },
+			{ x: -150, z: 20, density: 120, radius: 48 },
+			{ x: 150, z: 150, density: 125, radius: 53 },
+		];
+
+		groveCenters.forEach((grove) => {
+			for (let i = 0; i < grove.density; i++) {
+				const angle = Math.random() * 2 * Math.PI;
+				const distance = Math.random() * grove.radius;
+
+				const position = {
+					x: grove.x + distance * Math.cos(angle),
+					y: 0,
+					z: grove.z + distance * Math.sin(angle),
+				};
+
+				const rotation = {
+					x: 0,
+					y: Math.random() * 2 * Math.PI,
+					z: 0,
+				};
+
+				const scaleFactor = 0.9 + Math.random() * 0.4; // Random scale between 0.9 and 1.3
+				const scale = {
+					x: scaleFactor,
+					y: scaleFactor + (Math.random() * 0.2 - 0.1), //Slightly different Y scale
+					z: scaleFactor,
+				};
+
+				const name = models[Math.floor(Math.random() * models.length)];
+
+				natureObjects.push({ name, position, rotation, scale });
+			}
+		});
+
+		console.log(
+			`[procedural_generator.js] Generated ${natureObjects.length} objects.`
+		);
+		return natureObjects;
+	},
+};
+
+export { _proceduralGenerator };
